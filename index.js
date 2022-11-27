6fd7d21c2550440f10d2fde5616ac34fd1a0414f
bbecquet <benjamin.becquet@gmail.com>
2022-11-27 11:46:37 +0100

    Version and style updates

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 /* globals fetch, L, R */
 
 var getJson = function getJson(url) {
-    return fetch(url).then(function (response) {
-        return response.json();
-    });
+  return fetch(url).then(function (response) {
+    return response.json();
+  });
 };
 
 var humanizeTuneName = R.ifElse(R.endsWith(', The'), R.compose(R.concat('The '), R.dropLast(5)), R.identity);
@@ -15,95 +15,95 @@ var placeTunes = R.compose(R.defaultTo([]), R.path(['properties', 'tunes']));
 var nbTunes = R.compose(R.length, placeTunes);
 var tuneName = R.compose(humanizeTuneName, R.head, R.prop('names'));
 var compareTune = R.comparator(function (a, b) {
-    return tuneName(a).localeCompare(tuneName(b)) < 0;
+  return tuneName(a).localeCompare(tuneName(b)) < 0;
 });
 
 var warmRed = '#A52A2A';
 
 var normalStyle = {
-    color: warmRed,
-    fillColor: warmRed,
-    opacity: 0.7,
-    fillOpacity: 0.2
+  className: 'placeMarker',
+  fillColor: warmRed,
+  fillOpacity: 0.4,
+  weight: 0
 };
 
 var highlightedStyle = {
-    color: 'white',
-    fillColor: warmRed,
-    opacity: 1,
-    fillOpacity: 1
+  fillColor: warmRed,
+  opacity: 1,
+  fillOpacity: 0.9
 };
 
 var hoverStyle = {
-    color: warmRed,
-    fillColor: warmRed,
-    opacity: 1,
-    fillOpacity: 0.6
+  color: warmRed,
+  fillColor: warmRed,
+  opacity: 1,
+  fillOpacity: 0.6,
+  weight: 1
 };
 
 var createPlaceMarker = function createPlaceMarker(place, latLng) {
-    return L.circleMarker(latLng, R.merge(normalStyle, { radius: nbTunes(place) / 5 * 2 + 3 }));
+  return L.circleMarker(latLng, R.mergeLeft(normalStyle, { radius: nbTunes(place) / 5 * 2 + 3 }));
 };
 
 var formatTuneListItem = function formatTuneListItem(tune) {
-    return '<li><a href="https://thesession.org/tunes/' + tune.id + '">' + tuneName(tune) + '</a></li>';
+  return '<li><a href="https://thesession.org/tunes/' + tune.id + '">' + tuneName(tune) + '</a></li>';
 };
 
 var formatTuneList = R.pipe(R.sort(compareTune), R.map(formatTuneListItem), R.prepend('<ul>'), R.append('</ul>'), R.join(''));
 
 function displayTunes(place) {
-    var tunesByType = R.pipe(placeTunes, R.groupBy(R.prop('type')), R.toPairs, // => [type, tunes]
-    R.sortBy(R.head), R.map(function (group) {
-        return R.concat('<h3>' + group[0] + '</h3>', formatTuneList(group[1]));
-    }), R.join(''))(place);
+  var tunesByType = R.pipe(placeTunes, R.groupBy(R.prop('type')), R.toPairs, // => [type, tunes]
+  R.sortBy(R.head), R.map(function (group) {
+    return R.concat('<h3>' + group[0] + '</h3>', formatTuneList(group[1]));
+  }), R.join(''))(place);
 
-    document.getElementById('results').innerHTML = '<h2>' + placeName(place) + '</h2>' + tunesByType;
+  document.getElementById('results').innerHTML = '<h2>' + placeName(place) + '</h2>' + tunesByType;
 }
 
 function toGeoJsonLayer(places) {
-    var highlightedMarker = null;
+  var highlightedMarker = null;
 
-    return L.geoJson(places, {
-        pointToLayer: createPlaceMarker,
-        onEachFeature: function onEachFeature(feature, layer) {
-            layer.on('click', function () {
-                displayTunes(feature);
-                if (highlightedMarker) {
-                    highlightedMarker.setStyle(normalStyle);
-                }
-                highlightedMarker = layer;
-                layer.setStyle(highlightedStyle);
-            }).on('mouseover', function () {
-                if (layer === highlightedMarker) {
-                    return;
-                }
-                layer.setStyle(hoverStyle);
-            }).on('mouseout', function () {
-                if (layer === highlightedMarker) {
-                    return;
-                }
-                layer.setStyle(normalStyle);
-            }).bindTooltip(placeName(feature) + ' (' + nbTunes(feature) + ')', {
-                direction: 'top'
-            });
+  return L.geoJson(places, {
+    pointToLayer: createPlaceMarker,
+    onEachFeature: function onEachFeature(feature, layer) {
+      layer.on('click', function () {
+        displayTunes(feature);
+        if (highlightedMarker) {
+          highlightedMarker.setStyle(normalStyle);
         }
-    });
+        highlightedMarker = layer;
+        layer.setStyle(highlightedStyle);
+      }).on('mouseover', function () {
+        if (layer === highlightedMarker) {
+          return;
+        }
+        layer.setStyle(hoverStyle);
+      }).on('mouseout', function () {
+        if (layer === highlightedMarker) {
+          return;
+        }
+        layer.setStyle(normalStyle);
+      }).bindTooltip(placeName(feature) + ' (' + nbTunes(feature) + ')', {
+        direction: 'top'
+      });
+    }
+  });
 }
 
 function init() {
-    var map = L.map('map', {
-        center: [53.6, -7.86],
-        zoom: 7,
-        layers: [L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        })],
-        minZoom: 6,
-        maxBounds: L.latLngBounds([45, -15], [60, 0])
-    });
+  var map = L.map('map', {
+    center: [53.6, -7.86],
+    zoom: 7,
+    layers: [L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    })],
+    minZoom: 6,
+    maxBounds: L.latLngBounds([45, -15], [60, 0])
+  });
 
-    getJson('data/tunesByPlaces.geojson').then(function (places) {
-        map.addLayer(toGeoJsonLayer(places));
-    });
+  getJson('data/tunesByPlaces.geojson').then(function (places) {
+    map.addLayer(toGeoJsonLayer(places));
+  });
 }
 
 window.onload = init;
--- src/index.js
+++ src/index.js
@@ -1,111 +1,119 @@
 /* globals fetch, L, R */
 
-const getJson = url => fetch(url).then(response => response.json());
+const getJson = url => fetch(url).then(response => response.json())
 
 const humanizeTuneName = R.ifElse(
-    R.endsWith(', The'),
-    R.compose(R.concat('The '), R.dropLast(5)),
-    R.identity
-);
+  R.endsWith(', The'),
+  R.compose(R.concat('The '), R.dropLast(5)),
+  R.identity
+)
 
-const placeName = R.path(['properties', 'name']);
-const placeTunes = R.compose(R.defaultTo([]), R.path(['properties', 'tunes']));
-const nbTunes = R.compose(R.length, placeTunes);
-const tuneName = R.compose(humanizeTuneName, R.head, R.prop('names'));
-const compareTune = R.comparator((a, b) => tuneName(a).localeCompare(tuneName(b)) < 0);
+const placeName = R.path(['properties', 'name'])
+const placeTunes = R.compose(R.defaultTo([]), R.path(['properties', 'tunes']))
+const nbTunes = R.compose(R.length, placeTunes)
+const tuneName = R.compose(humanizeTuneName, R.head, R.prop('names'))
+const compareTune = R.comparator((a, b) => tuneName(a).localeCompare(tuneName(b)) < 0)
 
-const warmRed = '#A52A2A';
+const warmRed = '#A52A2A'
 
 const normalStyle = {
-    color: warmRed,
-    fillColor: warmRed,
-    opacity: 0.7,
-    fillOpacity: 0.2,
-};
+  className: 'placeMarker',
+  fillColor: warmRed,
+  fillOpacity: 0.4,
+  weight: 0,
+}
 
 const highlightedStyle = {
-    color: 'white',
-    fillColor: warmRed,
-    opacity: 1,
-    fillOpacity: 1,
-};
+  fillColor: warmRed,
+  opacity: 1,
+  fillOpacity: 0.9,
+}
 
 const hoverStyle = {
-    color: warmRed,
-    fillColor: warmRed,
-    opacity: 1,
-    fillOpacity: 0.6,
-};
+  color: warmRed,
+  fillColor: warmRed,
+  opacity: 1,
+  fillOpacity: 0.6,
+  weight: 1,
+}
 
-const createPlaceMarker = (place, latLng) => L.circleMarker(latLng,
-    R.merge(normalStyle, { radius: nbTunes(place) / 5 * 2 + 3 })
-);
+const createPlaceMarker = (place, latLng) =>
+  L.circleMarker(latLng, R.mergeLeft(normalStyle, { radius: (nbTunes(place) / 5) * 2 + 3 }))
 
 const formatTuneListItem = tune =>
-    `<li><a href="https://thesession.org/tunes/${tune.id}">${tuneName(tune)}</a></li>`;
+  `<li><a href="https://thesession.org/tunes/${tune.id}">${tuneName(tune)}</a></li>`
 
 const formatTuneList = R.pipe(
-    R.sort(compareTune),
-    R.map(formatTuneListItem),
-    R.prepend('<ul>'), R.append('</ul>'),
-    R.join('')
-);
+  R.sort(compareTune),
+  R.map(formatTuneListItem),
+  R.prepend('<ul>'),
+  R.append('</ul>'),
+  R.join('')
+)
 
 function displayTunes(place) {
-    const tunesByType = R.pipe(
-        placeTunes,
-        R.groupBy(R.prop('type')),
-        R.toPairs,  // => [type, tunes]
-        R.sortBy(R.head),
-        R.map(group => R.concat(`<h3>${group[0]}</h3>`, formatTuneList(group[1]))),
-        R.join('')
-    )(place);
+  const tunesByType = R.pipe(
+    placeTunes,
+    R.groupBy(R.prop('type')),
+    R.toPairs, // => [type, tunes]
+    R.sortBy(R.head),
+    R.map(group => R.concat(`<h3>${group[0]}</h3>`, formatTuneList(group[1]))),
+    R.join('')
+  )(place)
 
-    document.getElementById('results').innerHTML = `<h2>${placeName(place)}</h2>${tunesByType}`;
+  document.getElementById('results').innerHTML = `<h2>${placeName(place)}</h2>${tunesByType}`
 }
 
 function toGeoJsonLayer(places) {
-    let highlightedMarker = null;
+  let highlightedMarker = null
 
-    return L.geoJson(places, {
-        pointToLayer: createPlaceMarker,
-        onEachFeature: (feature, layer) => {
-            layer.on('click', () => {
-                displayTunes(feature);
-                if (highlightedMarker) {
-                    highlightedMarker.setStyle(normalStyle);
-                }
-                highlightedMarker = layer;
-                layer.setStyle(highlightedStyle);
-            }).on('mouseover', () => {
-                if(layer === highlightedMarker) { return; }
-                layer.setStyle(hoverStyle);
-            }).on('mouseout', () => {
-                if(layer === highlightedMarker) { return; }
-                layer.setStyle(normalStyle);
-            }).bindTooltip(`${placeName(feature)} (${nbTunes(feature)})`, {
-                direction: 'top'
-            });
-        }
-    });
+  return L.geoJson(places, {
+    pointToLayer: createPlaceMarker,
+    onEachFeature: (feature, layer) => {
+      layer
+        .on('click', () => {
+          displayTunes(feature)
+          if (highlightedMarker) {
+            highlightedMarker.setStyle(normalStyle)
+          }
+          highlightedMarker = layer
+          layer.setStyle(highlightedStyle)
+        })
+        .on('mouseover', () => {
+          if (layer === highlightedMarker) {
+            return
+          }
+          layer.setStyle(hoverStyle)
+        })
+        .on('mouseout', () => {
+          if (layer === highlightedMarker) {
+            return
+          }
+          layer.setStyle(normalStyle)
+        })
+        .bindTooltip(`${placeName(feature)} (${nbTunes(feature)})`, {
+          direction: 'top',
+        })
+    },
+  })
 }
 
 function init() {
-    const map = L.map('map', {
-        center: [53.6, -7.86],
-        zoom: 7,
-        layers: [
-            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            })
-        ],
-        minZoom: 6,
-        maxBounds: L.latLngBounds([45, -15], [60, 0])
-    });
+  const map = L.map('map', {
+    center: [53.6, -7.86],
+    zoom: 7,
+    layers: [
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }),
+    ],
+    minZoom: 6,
+    maxBounds: L.latLngBounds([45, -15], [60, 0]),
+  })
 
-    getJson('data/tunesByPlaces.geojson').then(places => {
-        map.addLayer(toGeoJsonLayer(places));
-    });
+  getJson('data/tunesByPlaces.geojson').then(places => {
+    map.addLayer(toGeoJsonLayer(places))
+  })
 }
 
-window.onload = init;
+window.onload = init
